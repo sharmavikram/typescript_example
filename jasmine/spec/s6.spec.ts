1998e1d7ae5b8e66b992b55b06a8df13b0642071
Vikram <sharmavikram054@gmail.com>
2020-01-04 11:35:37 +0530

    wirte a sample test case for spy function

---
+++ jasmine/spec/s6.spec.ts
@@ -0,0 +1,60 @@
+/**
+ * Spies function
+ * apies allows us to test to see whether a particular function was called. also 
+ * determine the actual parameters it was called with 
+ * 
+ * function name: spyOn, callFake
+ * call fake function override the return value, basically used for small mocking
+ */
+
+ class mySpcifiedClass {
+     testFunction(name: string) {
+         console.log(name);
+     }
+
+     doCallBack(id: number, callback: (result: string)=>void) {
+        let callbackId = "id: " + id.toString();
+        callback(callbackId);
+     }
+ }
+
+ class doCallback {
+     callbackLog(value: string) {
+         console.log(value);
+     }
+     getValue(): number {
+         return 5;
+     }
+ }
+ describe("spy function",()=>{
+     it("function should called with params",()=>{
+        let t1= new mySpcifiedClass();
+        let spy = spyOn(t1, "testFunction");
+        t1.testFunction("hello");
+        expect(spy).toHaveBeenCalled();
+     })
+     /**********************spying on callback function *********************/
+
+     it("spying on callback function", ()=>{
+        let d1 = new doCallback();
+        let t2 = new mySpcifiedClass();
+        let cspy = spyOn(d1, "callbackLog");
+
+        t2.doCallBack(1, d1.callbackLog);
+        expect(cspy).toHaveBeenCalled();
+        expect(cspy).toHaveBeenCalledWith("id: 1");
+     });
+
+     /**********************spying using fake function *********************/
+     it("fake function",() => {
+            let d2 = new doCallback();
+            spyOn(d2, "getValue").and.callFake( () => {
+                return 2
+            })
+            expect(d2.getValue()).toBe(2);
+     })
+ })
+
+
+
+
